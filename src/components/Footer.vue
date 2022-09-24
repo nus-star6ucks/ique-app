2919a339b670b2f4714df16ed45f6f9515662e5e
PCDotFan <i@g4.cx>
2022-09-25 00:59:00 +0800

    cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,10 +14,6 @@ const toggleLocales = () => {
       <div i-carbon-campsite />
     </RouterLink>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
-
     <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
       <div i-carbon-language />
     </a>
