58f2dfea100868365c2b41405e1b7b118958a52c
PCDotFan <i@g4.cx>
2022-10-02 00:52:12 +0800

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
-import 'virtual:windi.css'
-import './styles/main.css'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { createHead } from '@vueuse/head'
 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'virtual:windi.css'
+import './styles/main.css'
+
+const app = createApp(App)
+const head = createHead()
+
+const routes = setupLayouts(generatedRoutes)
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(router)
+
+app.use(head)
+app.mount('#app')
