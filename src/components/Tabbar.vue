86b32cae3b00bb0a0c16c7095f2437b85a04211f
PCDotFan <i@g4.cx>
2022-10-04 23:59:09 +0800

    feat: demo notification dot added.

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -14,7 +14,11 @@ export default {
       <span class="icon"><HomeIcon class="w-6 h-6" /></span>
       <span class="text">Home</span>
     </RouterLink>
-    <RouterLink to="/tickets" class="tab">
+    <RouterLink to="/tickets" class="tab relative">
+      <span class="absolute -right-1 top-0 flex h-2 w-2">
+        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-rose-400 opacity-75" />
+        <span class="relative inline-flex rounded-full h-2 w-2 bg-rose-400" />
+      </span>
       <span class="icon">
         <TicketIcon class="w-6 h-6" />
       </span>
