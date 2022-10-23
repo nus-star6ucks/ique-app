b5697b050b16ab861fc50a6ba299a6a947d661ff
PCDotFan <i@g4.cx>
2022-10-23 14:27:34 +0800

    cleanup

--- src/pages/tickets/[id].vue
+++ src/pages/tickets/[id].vue
@@ -50,7 +50,7 @@ const ticket = computed(() => data.value?.ticket)
           <h3 class="text-center text-gray-400 mb-2 text-xl">
             Your Queue Number
           </h3>
-          <p class="text-center text-5xl font-bold text-emerald-500" v-text="(ticket.queueInfo.waitingSize - 1) > 0 ? ticket.queueNumber : `It's Your Turn!`" />
+          <p class="text-center text-5xl font-bold text-emerald-500" v-text="(ticket.queueInfo.waitingSize - 1) > 0 ? ticket.queueNumber : 'It\'s Your Turn!'" />
         </div>
         <div class="grid grid-cols-3">
           <div class="px-4 py-8">
@@ -63,7 +63,7 @@ const ticket = computed(() => data.value?.ticket)
             <h4 class="text-gray-400 mb-2 text-base">
               Ahead
             </h4>
-            <p class="text-gray-800 text-xl font-semibold" v-text="`${ticket.queueInfo.waitingSize - 1} pax`" />
+            <p class="text-gray-800 text-xl font-semibold" v-text="`${ticket.queueInfo.waitingSize - 1} people`" />
           </div>
           <div class="px-4 py-8">
             <h4 class="text-gray-400 mb-2 text-base">
