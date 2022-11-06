8cff5bad6130aa7af9948b65aa17329ec3646a71
PCDotFan <i@g4.cx>
2022-11-07 03:06:39 +0800

    feat(oauth): github & google auth added.

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ArrowLeftIcon, LockClosedIcon, UserIcon } from '@heroicons/vue/24/outline/index.js'
 import xss from 'xss'
+import { Icon } from '@iconify/vue'
 import { useUserStore } from '~/stores/user'
 import { useSnackStore } from '~/stores/snack'
 import { hash, userApi } from '~/utils'
@@ -13,8 +14,34 @@ const username = ref<string>('')
 const password = ref<string>('')
 const loading = ref<boolean>(false)
 
-async function loginWithGitHub() {
-  window.location.href = '/api/oauth2/authorization/github'
+async function loginWithOAuth(strategy: 'google' | 'github') {
+  const oauthWindowHandle = window.open(`/api/oauth2/authorization/${strategy}`)
+
+  const polling = setInterval(() => {
+    if (!oauthWindowHandle) {
+      clearInterval(polling)
+      return
+    }
+
+    loading.value = true
+    const { href } = oauthWindowHandle.location
+    if (href.includes('/api/login/oauth2/code')) {
+      try {
+        const { innerText } = oauthWindowHandle.document.documentElement
+        if (!innerText)
+          throw new Error('Not a valid response')
+        const possibleJson: { token: string; userType: string } = JSON.parse(innerText)
+        userStore.login(possibleJson.token)
+        window.location.reload()
+      }
+      catch {}
+      finally {
+        loading.value = false
+        clearInterval(polling)
+        oauthWindowHandle.close()
+      }
+    }
+  }, 500)
 }
 
 async function onSubmit() {
@@ -78,15 +105,23 @@ async function onSubmit() {
         </div>
       </div>
       <div class="space-y-4">
-        <button type="button" :disabled="loading" :class="{ 'shadow-none': loading, 'bg-gray-400': loading }" class="transition-all w-full flex space-x-2 items-center justify-center bg-gray-900 shadow-md shadow-gray-500/40 text-white rounded-lg uppercase py-4" @click="loginWithGitHub">
-          <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-white"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z" /></svg>
-          <span>
-            Login With GitHub
-          </span>
-        </button>
         <button type="submit" :disabled="loading" :class="{ 'shadow-none': loading, 'bg-gray-400': loading }" class="transition-all text-center w-full bg-emerald-500 shadow-md shadow-emerald-500/40 text-white rounded-lg uppercase py-4">
           Login
         </button>
+        <div class="flex items-center justify-center space-x-4">
+          <button type="button" :disabled="loading" :class="{ 'shadow-none': loading, 'bg-gray-400': loading }" class="transition-all w-full flex space-x-2 items-center justify-center bg-gray-200 text-gray-500 hover:bg-gray-800 hover:text-white rounded-lg uppercase py-4" @click="loginWithOAuth('google')">
+            <Icon icon="akar-icons:github-fill" />
+            <span>
+              Login With GitHub
+            </span>
+          </button>
+          <button type="button" :disabled="loading" :class="{ 'shadow-none': loading, 'bg-gray-400': loading }" class="transition-all w-full flex space-x-2 items-center justify-center bg-gray-200 text-gray-500 hover:bg-sky-500 hover:text-white rounded-lg uppercase py-4" @click="loginWithOAuth('github')">
+            <Icon icon="akar-icons:google-fill" />
+            <span>
+              Login With Google
+            </span>
+          </button>
+        </div>
         <p class="text-center mt-4 text-gray-400 px-4 text-xs">
           By clicking Login, you agree to our <a href="javascript:;">Terms and Conditions</a> and <a href="javascript:;">Privacy Statement</a>
         </p>
